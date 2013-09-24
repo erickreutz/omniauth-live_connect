aed3239487e143900a586a208a2f47423a6ce2a4
Sujith Radhakrishnan <sujith@switchpt.com>
2013-09-24 17:37:14 -0400

    Adding scope support

--- lib/omniauth/live_connect/strategies/live_connect.rb
+++ lib/omniauth/live_connect/strategies/live_connect.rb
@@ -4,6 +4,8 @@ module OmniAuth
   module Strategies
     class LiveConnect < OmniAuth::Strategies::OAuth2
 
+      DEFAULT_SCOPE = 'wl.signin'
+
       option :name,   "live_connect"
       option :client_options, { site:           'https://login.live.com',
                                 authorize_url:  '/oauth20_authorize.srf',
@@ -13,6 +15,27 @@ module OmniAuth
       uid{ raw_info['id'] }
 
       info { raw_info }
+
+      ##
+      # You can pass +state+, +scope+ params to the auth request, if
+      # you need to set them dynamically. 
+      #
+      # /auth/live_connect?scope=wl.skydrive_update,wl.offline_access&state=ABC
+      #
+      def authorize_params
+        super.tap do |params|
+          %w[state scope].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+
+              # to support omniauth-oauth2's auto csrf protection
+              session['omniauth.state'] = params[:state] if v == 'state'
+            end
+          end
+
+          params[:scope] ||= DEFAULT_SCOPE
+        end
+      end
 
       private
         def raw_info
